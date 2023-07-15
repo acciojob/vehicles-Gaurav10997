870b9972076259ae140daa61f016a48338b4c880
gaurav mandal <avdhika@gmail.com>
2023-07-15 17:43:49 +0530

    hey

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,22 @@
+package com.driver;
+
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(){
+
+    }
+    public Boat(String name,int capacity){
+        this.capacity = capacity;
+        this.name = name;
+    }
+    public String getVehicleName() {
+        return name;
+    }
+
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
